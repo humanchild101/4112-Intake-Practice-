17deacd8fc374bb89acf9c2160a756699ed9f5c5
mahiatm <77015554+mahiatm@users.noreply.github.com>
2025-11-09 21:23:07 -0500

    Update IntakeIOReal.java

--- src/main/java/frc/robot/Intake/IntakeIOReal.java
+++ src/main/java/frc/robot/Intake/IntakeIOReal.java
@@ -1,1 +1,30 @@
 
+public class IntakeIOReal implements IntakeIO{
+
+    public IntakeIOReal(){
+
+    }
+    
+    @Override
+    public void updateInputs(){
+        var elev1Status = BaseStatusSignal.refreshAll(elevVol, elevPos, elevVel, elevCur);
+
+    }
+
+    @Override
+    public void setIntakeClosedLoop(double pos){ 
+
+    }
+
+    @Override
+    public void setIntakeOpenLoop(double volts){
+    }
+
+    @Override
+    public void resetState() {
+    }
+
+    @Override
+    public void stopIntake(){
+    }
+}
