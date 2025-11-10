2cc9ea9aee85d5219a30a61ee114dff9ec863755
mahiatm <77015554+mahiatm@users.noreply.github.com>
2025-11-09 21:21:23 -0500

    Update IntakeIO.java

--- src/main/java/frc/robot/Intake/IntakeIO.java
+++ src/main/java/frc/robot/Intake/IntakeIO.java
@@ -1,1 +1,20 @@
 
+
+public interface IntakeIO{
+    @AutoLog
+    public static class IntakeIOInputs{
+
+
+    }
+
+    public default void updateInputs(){}
+
+    public default void setIntakeClosedLoop(){}
+    
+    public default void setIntakeOpenLoop(){}
+
+    public default void stopIntake(){} 
+
+	public default void resetState(){} 
+}
+   
