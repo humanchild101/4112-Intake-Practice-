10112c0348042b43329e5d989d0c700c4e213cb0
mahiatm <77015554+mahiatm@users.noreply.github.com>
2025-11-09 21:17:15 -0500

    Update IntakeConstants.java

--- src/main/java/frc/robot/Intake/IntakeConstants.java
+++ src/main/java/frc/robot/Intake/IntakeConstants.java
@@ -1,1 +1,24 @@
 
+public class ElevatorConstants {
+    public enum ElevatorPosition {
+        M1START(0.0),
+        M1HOLD(0.0),
+        M1ROTATE(0.0),
+        M2START(0.0),
+        M2HOLD(0.0),
+        M2ROTATE(0.0),
+       
+
+        public final double value;
+        private MotorOnePosition(double value){
+            this.value = value;
+         }
+
+        private MotorTwoPosition(double value){
+            this.value = value;
+         }
+    }
+
+
+    
+
