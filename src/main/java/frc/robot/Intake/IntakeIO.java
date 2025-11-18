c6c0d62f804b98e627f014d27296eb080ec43562
TieGuyGaming <doctortieguy@gmail.com>
2025-11-18 07:43:14 -0500

    Intake Commit 2 : Electric Boogalo
    Added inputs to the intake Io, and added some motor definitions and other methods inherited from intakeIO to intakeIO real

--- src/main/java/frc/robot/Intake/IntakeIO.java
+++ src/main/java/frc/robot/Intake/IntakeIO.java
@@ -2,7 +2,19 @@ package frc.robot.Intake;
 
 public interface IntakeIO {
 
+
+    public static class IntakeIOInputs{
+        public boolean raiserConnected = false;
+        public boolean indexerConnected = false;
+        public double indexerVoltage = 0;
+        public double raiserVoltage = 0;
+        
+    }
+
     public default void setIndexerVoltage(){
+
+    }
+    public default void setRaiserVoltage(){
 
     }
 
@@ -12,11 +24,12 @@ public interface IntakeIO {
     public default void setRaiserOpenLoop(){
 
     }
+   
 
     public default void resetState(){
 
     }
-    public default void updateInputs(){
+    public default void updateInputs(IntakeIOInputs io){
         //the inputs that i definitely have
     }
 
--- src/main/java/frc/robot/Intake/IntakeIOReal.java
+++ src/main/java/frc/robot/Intake/IntakeIOReal.java
@@ -1,6 +1,35 @@
 package frc.robot.Intake;
 
+import com.ctre.phoenix6.hardware.TalonFX;
+
 public class IntakeIOReal implements IntakeIO{
-    
+    public final TalonFX raiserMotor, indexerMotor;
+
+
+
+    @Override
+    public void resetState() {
+        
+    }
+    @Override
+    public void setIndexerVoltage() {
+        
+    }
+    @Override
+    public void setRaiserClosedLoop() {
+     
+    }
+    @Override
+    public void setRaiserOpenLoop() {
+    }
+
+    @Override
+    public void setRaiserVoltage() {
+       
+    }
+    @Override
+    public void updateInputs(IntakeIOInputs io) {
+        
+    }
     
 }
